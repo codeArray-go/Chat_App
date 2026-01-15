5bfcdb0c6f9dba4aa21c53bbd97025487d25998b
codeArray-go <loneranger2520@gmail.com>
2026-01-15 14:39:38 +0530

    ready to deployment

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,10 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
+
+// Make web ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Front-end/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Front-end", "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
