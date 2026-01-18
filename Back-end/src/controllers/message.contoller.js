6cb566115bbc681ef9fba9e074ba0f5887d0cc96
codeArray-go <loneranger2520@gmail.com>
2026-01-18 15:13:14 +0530

    message routes optimized

--- Back-end/src/controllers/message.contoller.js
+++ Back-end/src/controllers/message.contoller.js
@@ -40,6 +40,19 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
+
     let imageUrl;
     if (image) {
       const uploadToCloudinary = await cloudinary.uploader.upload(image);
@@ -88,7 +101,6 @@ export const getChatParameter = async (req, res) => {
     }).select("-password");
 
     res.status(200).json(chatParteners);
-
   } catch (error) {
     console.log("Error in getChatparameter: ", error.message);
     res.status(500).json({ error: "Internal server error." });
