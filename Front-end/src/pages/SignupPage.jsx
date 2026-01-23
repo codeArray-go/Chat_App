74200a3f4595f6355b67d37bf01c1a704273538f
codeArray-go <loneranger2520@gmail.com>
2026-01-23 11:52:59 +0530

    Tailwind config file removed and animation updated in index.css

--- Front-end/src/pages/SignupPage.jsx
+++ Front-end/src/pages/SignupPage.jsx
@@ -55,7 +55,7 @@ function SignUpPage() {
                           setFormData({ ...formData, fullName: e.target.value })
                         }
                         className="input"
-                        placeholder="John Doe"
+                        placeholder="Billu Bhasanda"
                       />
                     </div>
                   </div>
@@ -73,7 +73,7 @@ function SignUpPage() {
                           setFormData({ ...formData, email: e.target.value })
                         }
                         className="input"
-                        placeholder="johndoe@gmail.com"
+                        placeholder="billubhasanda00@gmail.com"
                       />
                     </div>
                   </div>
