c3ebf8f05375fc8c19a12b5d4c1a0b3b31cc0b9e
codeArray-go <loneranger2520@gmail.com>
2026-01-28 22:02:21 +0530

    jwt configuration upgraded

--- Back-end/src/lib/utils.js
+++ Back-end/src/lib/utils.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const genrateToken = (userId, res) => {
-  const { JWT_SECRET, NODE_ENV } = ENV;
+  const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not set");
   }
@@ -15,7 +15,7 @@ export const genrateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //this is in millisecond
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "strict", //CSRF attacks
-    secure: NODE_ENV !== "development",
+    secure: true, // for production purpose
     path: "/",
   });
 
