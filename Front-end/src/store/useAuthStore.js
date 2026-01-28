b8f1aa5acf957d00cfb2b0e50c272c08ba383103
Akhil singh Bhandari <loneranger2520@gmail.com>
2026-01-28 22:21:40 +0530

    Update useAuthStore.js

--- Front-end/src/store/useAuthStore.js
+++ Front-end/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : `${process.env.NEXT_PUBLIC_BACKEND_URL}/`;
+    : "https://chween.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
