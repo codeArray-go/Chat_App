778d8a690df6451c872c12b24a2005e6b99ebf47
codeArray-go <loneranger2520@gmail.com>
2026-01-31 23:02:46 +0530

    real time message typing tracking functionality added.

--- Back-end/src/lib/utils.js
+++ Back-end/src/lib/utils.js
@@ -15,7 +15,9 @@ export const genrateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //this is in millisecond
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "strict", //CSRF attacks
-    secure: true, // for production purpose
+    // sameSite: "none", // for deployment of different platforms
+    // secure: true, // for production purpose
+    // secure: ,
     path: "/",
   });
 
