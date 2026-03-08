15e82f577787b42d0c52700d89a8ec5e858c6ff4
Akhil singh Bhandari <loneranger2520@gmail.com>
2026-03-08 20:30:00 +0530

    Manage body overflow on App component mount
    Added effect to manage body overflow on mount and unmount.

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -24,6 +24,13 @@ const App = () => {
     UseChatStore.getState().initTypingListener();
   }, [authUser]);
 
+    useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   if (isCheckingAuth) return <PageLoader />;
 
   return (
@@ -54,3 +61,4 @@ const App = () => {
 };
 
 export default App;
+
