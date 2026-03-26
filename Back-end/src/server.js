8378eda0e5620b8bc691b9aa8bd609a594c3a8c0
Akhil singh Bhandari <loneranger2520@gmail.com>
2026-03-26 11:54:30 +0530

    Change ping response message in server.js
    Updated the ping response message to indicate server status.

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -23,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
 app.get("/api/ping", (req, res) => {
-  res.status(200).json({ message: "Server alive 🚀" });
+  res.status(200).json({ message: "Server is alive 👍🏻" });
 });
 
 const PORT = ENV.PORT;
