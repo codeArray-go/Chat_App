3e633f583331a511d958281bb56e8929b88898fb
Akhil singh Bhandari <loneranger2520@gmail.com>
2026-04-13 11:25:57 +0530

    Update axios base URL for production environment

--- Front-end/src/lib/axios.js
+++ Front-end/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://chween-bfz7.onrender.com/api",
+      : "https://chween-web.onrender.com/api",
   withCredentials: true,
 });
